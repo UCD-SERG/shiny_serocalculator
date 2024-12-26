42fb59f25f5ad806072f445e45bdef78f0a7e254
Chris Orwa <chrisorwa@gmail.com>
2024-12-26 13:12:14 +0300

    add more modules

--- R/import_data_module.R
+++ R/import_data_module.R
@@ -31,15 +31,13 @@ import_data_ui <- function(id) {
                     choices = c("Pop Data", "Curve Data", "Noise Data"),
                     selected = "Pop Data",
                     choicesOpt = list(disabled = c(FALSE, TRUE, TRUE))
-                    ),
+        ),
+
+        # how to upload pop_data ("OSF" | "File Upload")
+        uiOutput(ns("pop_type")),
 
         # file upload
-        fileInput(ns("file_upload"),
-                  "Choose File from Computer (.csv, .rds)",
-                  buttonLabel = "Upload...",
-                  multiple = TRUE,
-                  accept = c(".csv", ".rds")
-        ),
+        uiOutput(ns("pop_upload_type")),
 
         # File Upload Indicators
         div(
@@ -144,11 +142,80 @@ import_data_server <- function(id,
     curve_data <- reactiveVal(NULL)
     noise_data <- reactiveVal(NULL)
 
+    # MODULE 0: Choose how to get pop_data
+    # Select how to get pop data
+    observeEvent(input$data_upload_type, {
+      req(input$data_upload_type)
+
+      if (input$data_upload_type == "Pop Data") {
+        output$pop_type <- renderUI({
+          selectInput(
+            inputId = ns("pop_type_ext"),
+            label = "Choose Type",
+            choices = c("Upload", "OSF"),
+            selected = "Upload"
+          )
+        })
+      } else {
+        output$pop_type <- renderUI(NULL) # Ensures UI element is cleared for non "Pop Data" file names
+      }
+    })
+
+    # Dynamically render UI based on conditions
+    output$pop_upload_type <- renderUI({
+       req(input$pop_type_ext) # Ensure pop_type is available
+       req(input$data_upload_type) # Ensure file_name is available
+
+
+      if (input$data_upload_type == "Pop Data" ) {
+        if (input$pop_type_ext == "Upload") {
+          # File upload interface for Pop Data
+          fileInput(
+            inputId = ns("pop_upload"),
+            label = "Choose (Pop Data) File from Computer (.csv, .rds)",
+            buttonLabel = "Upload...",
+            multiple = TRUE,
+            accept = c(".csv", ".rds")
+          )
+        } else if (input$pop_type_ext == "OSF") {
+          # URL input and download button for OSF
+          tagList(
+            textInput(
+              inputId = ns("pop_data_url"),
+              label = "Provide OSF URL:"
+            ),
+            actionButton(
+              inputId = ns("pop_data_url_btn"),
+              label = "Download Data"
+            )
+          )
+        }
+      } else if (input$data_upload_type == "Curve Data") {
+        # File upload interface for Curve Data
+        fileInput(
+          inputId = ns("curve_upload"),
+          label = "Choose (Curve Data) File from Computer (.csv, .rds)",
+          buttonLabel = "Upload...",
+          multiple = TRUE,
+          accept = c(".csv", ".rds")
+        )
+      } else if (input$data_upload_type == "Noise Data") {
+        # File upload interface for Noise Data
+        fileInput(
+          inputId = ns("noise_upload"),
+          label = "Choose File from Computer (.csv, .rds)",
+          buttonLabel = "Upload...",
+          multiple = TRUE,
+          accept = c(".csv", ".rds")
+        )
+      }
+    })
+
 
     # MODULE 1: This should be module one (file upload)
-    observeEvent(input$file_upload, {
+    observeEvent(input$pop_upload, {
       req(input$data_upload_type == "Pop Data") # Check if "Pop Data" is selected
-      if (!is.null(input$file_upload)) {
+      if (!is.null(input$pop_upload)) {
         # Enable "Curve Data"
         updatePickerInput(
           session = session,
@@ -163,16 +230,16 @@ import_data_server <- function(id,
     })
 
     # Enable "Noise Data" after uploading "Curve Data"
-    observeEvent(input$file_upload, {
-      req(input$data_upload_type == "Curve Data") # Check if "Curve Data" is selected
-      if (!is.null(input$file_upload)) {
+    observeEvent(input$curve_upload, {
+      req(input$data_upload_type == "Curve Data")
+      if (!is.null(input$pop_upload)) {
         # Enable "Noise Data"
         updatePickerInput(
           session = session,
           inputId = "data_upload_type",
           choices = c("Pop Data", "Curve Data", "Noise Data"),
           choicesOpt = list(
-            disabled = c(FALSE, FALSE, FALSE) # Enable all
+            disabled = c(FALSE, FALSE, FALSE)
           ),
           selected = "Curve Data"
         )
@@ -180,90 +247,181 @@ import_data_server <- function(id,
     })
 
     # MODULE 2: Coloring file upload
-    observeEvent(input$file_upload, {
-      req(input$file_upload)
-      if (!is.null(input$file_upload) && input$data_upload_type == "Pop Data") {
+    observeEvent(c(input$noise_upload,
+                   input$pop_upload,
+                   input$curve_upload), {
+      if (!is.null(input$pop_upload) && input$data_upload_type == "Pop Data") {
         shinyjs::runjs('document.getElementById("pop_data_indicator").style.backgroundColor = "MediumSeaGreen";')
-      } else if (!is.null(input$file_upload) && input$data_upload_type == "Curve Data") {
+      } else if (!is.null(input$curve_upload) && input$data_upload_type == "Curve Data") {
         shinyjs::runjs('document.getElementById("curve_data_indicator").style.backgroundColor = "MediumSeaGreen";')
-      } else if (!is.null(input$file_upload) && input$data_upload_type == "Noise Data"){
+      } else if (!is.null(input$noise_upload) && input$data_upload_type == "Noise Data"){
         shinyjs::runjs('document.getElementById("noise_data_indicator").style.backgroundColor = "MediumSeaGreen";')
       } else {
         shinyjs::runjs('document.getElementById("status_circle").style.backgroundColor = "Tomato";')
       }
     })
 
-    # MODULE 3: Clear Environment
-    ## clear environment
-    observeEvent(input$clear_btn, {
-      req(input$clear_btn)
+  # MODULE 5: Updates reactive objects with files uploaded
+    # Observe changes in updatedData and uploaded files
+    observeEvent(c(
+      input$updatedData,
+      input$noise_upload,
+      input$curve_upload,
+      input$pop_upload
+    ), {
+      req(input$updatedData) # Ensure updatedData is available
 
-      # clear indicators
-      shinyjs::runjs('document.getElementById("pop_data_indicator").style.backgroundColor = "Tomato";')
-      shinyjs::runjs('document.getElementById("curve_data_indicator").style.backgroundColor = "Tomato";')
-      shinyjs::runjs('document.getElementById("noise_data_indicator").style.backgroundColor = "Tomato";')
+      output$head <- renderTable({
+        if (input$updatedData == "Noise Data") {
+          # Check if a file has been uploaded for Noise Data
+          req(input$noise_upload)
 
-      # clear file upload
-      updateFileInput(session, "file_upload", label = "Upload a File", value = NULL)
+          # Read the uploaded file using the helper function
+          df <- read_data_file(input$noise_upload)
 
-      # set reactive objects to NULL
-      pop_data(NULL)
-      curve_data(NULL)
-      noise_data(NULL)
+          # Update the reactiveVal with the new noise data
+          noise_data(df)
 
-      # clear enviroment
-      rm(list = ls())
+          head(noise_data()) # Display the head of the noise data
+        } else if (input$updatedData == "Curve Data") {
+          # Check if a file has been uploaded for Curve Data
+          req(input$curve_upload)
 
-      #clear dropdown files
-      uploaded_files$files <- setdiff(uploaded_files$files, "Pop Data")
-      uploaded_files$files <- setdiff(uploaded_files$files, "Noise Data")
-      uploaded_files$files <- setdiff(uploaded_files$files, "Curve Data")
+          # Read the uploaded file using the helper function
+          df <- read_data_file(input$curve_upload)
 
-      updateSelectInput(session, "selectedData", choices = uploaded_files$files)
-      updateSelectInput(session, "updatedData", choices = uploaded_files$files)
-      updateSelectInput(session, "updatedData_ext", choices = uploaded_files$files)
+          # Update the reactiveVal with the new curve data
+          curve_data(df)
 
+          head(curve_data()) # Display the head of the curve data
+        } else if (input$updatedData == "Pop Data") {
+          # Check if a file has been uploaded for Pop Data
+          req(input$pop_upload)
 
-      # clear outputs
-      output$est_incidence <- renderTable({ NULL})
-      output$stratify_by <- renderUI({NULL})
-      output$antigen_type <- renderUI({NULL})
-      output$visualize <- renderPlot({NULL})
-      output$stratification <- renderUI({NULL})
-      output$stratification <- renderUI({NULL})
-      output$other_head <- renderTable({NULL})
-      output$head <- renderTable({NULL})
-      output$numeric_summary <- renderTable({NULL})
+          # Read the uploaded file using the helper function
+          df <- read_data_file(input$pop_upload)
 
+          # Update the reactiveVal with the new pop data
+          pop_data(df) # Assuming pop_data is a reactiveVal
 
+          head(pop_data()) # Display the head of the pop data
+        }
+      })
     })
-  })
+
+  #   # MODULE 3: Clear Environment
+  #   ## clear environment
+  #   observeEvent(input$clear_btn, {
+  #     req(input$clear_btn)
+  #
+  #     # clear indicators
+  #     shinyjs::runjs('document.getElementById("pop_data_indicator").style.backgroundColor = "Tomato";')
+  #     shinyjs::runjs('document.getElementById("curve_data_indicator").style.backgroundColor = "Tomato";')
+  #     shinyjs::runjs('document.getElementById("noise_data_indicator").style.backgroundColor = "Tomato";')
+  #
+  #     # clear file upload
+  #     updateFileInput(session, "file_upload", label = "Upload a File", value = NULL)
+  #
+  #     # set reactive objects to NULL
+  #     pop_data(NULL)
+  #     curve_data(NULL)
+  #     noise_data(NULL)
+  #
+  #     # clear enviroment
+  #     rm(list = ls())
+  #
+  #     #clear dropdown files
+  #     uploaded_files$files <- setdiff(uploaded_files$files, "Pop Data")
+  #     uploaded_files$files <- setdiff(uploaded_files$files, "Noise Data")
+  #     uploaded_files$files <- setdiff(uploaded_files$files, "Curve Data")
+  #
+  #     updateSelectInput(session, "selectedData", choices = uploaded_files$files)
+  #     updateSelectInput(session, "updatedData", choices = uploaded_files$files)
+  #     updateSelectInput(session, "updatedData_ext", choices = uploaded_files$files)
+  #
+  #
+  #     # clear outputs
+  #     output$est_incidence <- renderTable({ NULL})
+  #     output$stratify_by <- renderUI({NULL})
+  #     output$antigen_type <- renderUI({NULL})
+  #     output$visualize <- renderPlot({NULL})
+  #     output$stratification <- renderUI({NULL})
+  #     output$stratification <- renderUI({NULL})
+  #     output$other_head <- renderTable({NULL})
+  #     output$head <- renderTable({NULL})
+  #     output$numeric_summary <- renderTable({NULL})
+  #
+  #
+  #   })
+  # })
 
   # MODULE 4: Update reactive objects to hold uploaded data
   # Observe changes in updatedData and uploaded files
-  observeEvent(c(
-    input$updatedData,
-    input$noise_upload,
-    input$curve_upload,
-    input$pop_upload,
-    input$file_upload
-  ), {
-    req(input$file_upload)
+  # observeEvent(c(
+  #   input$updatedData,
+  #   input$noise_upload,
+  #   input$curve_upload,
+  #   input$pop_upload,
+  #   input$file_upload
+  # ), {
+  #   req(input$file_upload)
+  #
+  #   output$head <- renderDT({
+  #     if (input$file_upload == "Noise Data") {
+  #       df <- read_data_file(input$file_upload)
+  #       noise_data(df)
+  #       head(noise_data())
+  #     } else if (input$file_upload == "Curve Data") {
+  #       df <- read_data_file(input$file_upload)
+  #       curve_data(df)
+  #       head(curve_data())
+  #     } else if (input$file_upload == "Pop Data") {
+  #       df <- read_data_file(input$file_upload)
+  #       pop_data(df)
+  #       head(pop_data())
+  #     }
+  #   })
 
-    output$head <- renderDT({
-      if (input$file_upload == "Noise Data") {
-        df <- read_data_file(input$file_upload)
-        noise_data(df)
-        head(noise_data())
-      } else if (input$file_upload == "Curve Data") {
-        df <- read_data_file(input$file_upload)
-        curve_data(df)
-        head(curve_data())
-      } else if (input$file_upload == "Pop Data") {
-        df <- read_data_file(input$file_upload)
-        pop_data(df)
-        head(pop_data())
-      }
+    output$data_requirement <- renderText({
+      HTML("<p> <strong> Required datasets </strong>
+
+    The following data is required to perform the analyis the analysis. The data sets can be uploaded from your personal computer in .csv or .rds format,
+    or linked from our OSF repositories (<a href=https://osf.io/ne8pc/>https://osf.io/ne8pc/</a>). The Noise Data can be uploaded, linked or entered manually.
+
+        <ul>
+          <li> <strong>Cross-sectional Population Data (Pop Data)</strong>
+                  <p>A dataset with one row per sample and columns for antigen isotype, quantitative antibody results, and age in years.</p>
+ <ul>
+              <li>Additional columns and variables can be included for stratification</li>
+              <li>Age unit is years, decimal points are fine</li>
+              <li>The scale of the antibody response variable must be the same as the longituidnal antibody decay data (curve data) </li>
+              <li>Do not upload any identifying health information </li>
+   </p>
+            </ul>
+          <li><strong>Antibody Decay Curve Data (Curve Data) </strong></li>
+          <p>A data set containing antibody decay curve parameters fit using a two-phase within-host
+          Bayesian hierarchical framework obtaining predictive posterior samples using Markov chain Monte Carlo sampling.
+          Note that variable names <u>must</u> follow these guidelines. For more information see <a href=https://onlinelibrary.wiley.com/doi/10.1002/sim.5322>Teunis (2012)</a>.
+          The scale of y0 and y1 must be the same as the antibody response variable in the population data frame</p>
+            <ul>
+              <li>y0: baseline antibody level</li>
+              <li>y1: antibody peak level </li>
+              <li>t1: time from symptom onset to peak antibody response (in days) </li>
+              <li>alpha: antibody decay rate (1/days for the current longitudinal parameter sets)</li>
+              <li>r: shape factor of antibody decay</li>
+            </ul>
+        </p>
+           <li> <strong>Noise Data</strong>
+                  <p>A dataset containing the following variables, specifying noise parameters for each antigen isotype.
+                  Note that variable names <u>must</u> follow these guidelines. For more information see <a hfref=https://onlinelibrary.wiley.com/doi/10.1002/sim.8578>Teunis (2020)</a>.
+                  <ul>
+                    <li>antigen_iso: antigen isotype whose noise parameters are being specified on each row</li>
+                    <li>nu: biological noise</li>
+                    <li>y.low: Lower limit of detection of the antibody assay</li>
+                    <li>y.high: Upper limit of detection of the antibody assay</li>
+                    <li>eps: measurement noise</li>
+                  </ul></p>
+         <p>File limit: <strong>500MB</strong></p>")
     })
   })
 
--- R/server.R
+++ R/server.R
@@ -5,50 +5,50 @@
 #' @importFrom utils read.csv
 
 server <- function(input, output, session) {
-  # Reactive object to hold uploaded data
-  data <- reactive({
-    req(input$upload)
-    ext <- tools::file_ext(input$upload$name)
-
-    # Read the data based on the file extension
-    switch(ext,
-      "csv" = read.csv(input$upload$datapath),
-      "rds" = readRDS(input$upload$datapath),
-      return(NULL) # Return NULL for unsupported extensions
-    )
-  })
-
-  # Initialize reactive values
-  uploaded_files <- reactiveValues(files = NULL)
-  pop_data <- reactiveVal(NULL)
-  curve_data <- reactiveVal(NULL)
-  noise_data <- reactiveVal(NULL)
-
-  data_df <- reactiveVal(NULL)
-  filename <- reactiveVal(NULL)
-
-  # Reactive object (data frame) for default noise values
-  noise_values <- reactiveValues(new_val = data.frame(
-    antigen = character(),
-    y_low = numeric(),
-    y_high = numeric(),
-    eps = numeric(),
-    nu = numeric(),
-    stringsAsFactors = FALSE
-  ))
-
-  # Get uploaded column names
-  column_names <- reactive({
-    req(input$upload)
-
-    null_file <- is.null(input$upload$datapath)
-    if (null_file) {
-      return(NULL)
-    }
-
-    df <- vroom::vroom(input$upload$datapath, delim = ",")
-    names(df)
-  })
+  # # Reactive object to hold uploaded data
+  # data <- reactive({
+  #   req(input$upload)
+  #   ext <- tools::file_ext(input$upload$name)
+  #
+  #   # Read the data based on the file extension
+  #   switch(ext,
+  #     "csv" = read.csv(input$upload$datapath),
+  #     "rds" = readRDS(input$upload$datapath),
+  #     return(NULL) # Return NULL for unsupported extensions
+  #   )
+  # })
+  #
+  # # Initialize reactive values
+  # uploaded_files <- reactiveValues(files = NULL)
+  # pop_data <- reactiveVal(NULL)
+  # curve_data <- reactiveVal(NULL)
+  # noise_data <- reactiveVal(NULL)
+  #
+  # data_df <- reactiveVal(NULL)
+  # filename <- reactiveVal(NULL)
+  #
+  # # Reactive object (data frame) for default noise values
+  # noise_values <- reactiveValues(new_val = data.frame(
+  #   antigen = character(),
+  #   y_low = numeric(),
+  #   y_high = numeric(),
+  #   eps = numeric(),
+  #   nu = numeric(),
+  #   stringsAsFactors = FALSE
+  # ))
+  #
+  # # Get uploaded column names
+  # column_names <- reactive({
+  #   req(input$upload)
+  #
+  #   null_file <- is.null(input$upload$datapath)
+  #   if (null_file) {
+  #     return(NULL)
+  #   }
+  #
+  #   df <- vroom::vroom(input$upload$datapath, delim = ",")
+  #   names(df)
+  # })
 
 
   # Call modules
@@ -62,15 +62,15 @@ server <- function(input, output, session) {
     noise_data = noise_data
   )
 
-  inspect_data_server(
-    id = "inspect_data",
-    dataReactive = data()
-  )
+  # inspect_data_server(
+  #   id = "inspect_data",
+  #   dataReactive = data()
+  # )
 
-  estimate_seroincidence_server(
-    id = "estimate_seroincidence",
-    pop_data = pop_data,
-    curve_data = curve_data,
-    noise_data = noise_data
-  )
+  # estimate_seroincidence_server(
+  #   id = "estimate_seroincidence",
+  #   pop_data = pop_data,
+  #   curve_data = curve_data,
+  #   noise_data = noise_data
+  # )
 }
