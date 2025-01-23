8b1531b2c64d4e178311aabbefac643feccadef8
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2025-01-22 20:32:54 -0800

    setting up golem

--- app.R
+++ app.R
@@ -1,4 +1,8 @@
+# Launch the ShinyApp (Do not remove this comment)
+# To deploy to shinyapps.io, run: rsconnect::deployApp()
+# Or use the blue button on top of this file
+reactlog::reactlog_enable()
 # Load the package
-pkgload::load_all()
-
-shinyApp(ui = ui, server = server)
+pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
+options( "golem.app.prod" = TRUE)
+shiny.serocalculator::run_app() # add parameters here (if any)
